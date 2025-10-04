818e3909459a1694c9d470516d79d96851663ccc
Agrovate <nishantullal@icloud.com>
2025-10-04 10:40:26 +0530

    adding a few keybinds

--- init.lua
+++ init.lua
@@ -1,11 +1,23 @@
-vim.g.mapleader = " "
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
+vim.opt.expandtab = true
 vim.opt.cursorline = true
 vim.opt.winborder = "rounded"
 vim.opt.smartindent = true
+vim.opt.guicursor = ""
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.wrap = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "80"
 
 vim.opt.packpath:prepend(vim.fn.stdpath('data') .. '/site')
 vim.pack.add({
@@ -108,6 +120,8 @@ require("blink.cmp").setup({
 
 -- Custom Keybinds
 vim.keymap.set('v', '<leader>y', '"+y', { desc = "Yank to system clipboard" })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 
 
