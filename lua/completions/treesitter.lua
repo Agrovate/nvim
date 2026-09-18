d6bae0a8c20589bc88df468d9e22a593a2a071de
agrovate <nishantullal@icloud.com>
2026-09-18 12:51:52 +0530

    error using old treesitter

--- lua/completions/treesitter.lua
+++ lua/completions/treesitter.lua
@@ -1,24 +1,24 @@
-require("nvim-treesitter.configs").setup({
-
+require("nvim-treesitter").setup({
     highlight = { enable = true },
     indent = { enable = true },
-    ensure_installed = {
-        "c",
-        "lua",
-        "vim",
-        "markdown",
-        "rust",
-        "cpp",
-        "python",
-        "go",
-        "qmljs",
-        "bash",
-        "html",
-        --        "latex",
-        "javascript",
-        "json",
-        "tsx",
-        "typescript",
-        "yaml",
-    },
+})
+
+require("nvim-treesitter").install({
+    "c",
+    "lua",
+    "vim",
+    "markdown",
+    "rust",
+    "cpp",
+    "python",
+    "go",
+    "qmljs",
+    "bash",
+    "html",
+    --        "latex",
+    "javascript",
+    "json",
+    "tsx",
+    "typescript",
+    "yaml",
 })
